f77601b74bb4a0e6f251b361bf96335576375282
Nathan Cerny <ncerny@chef.io>
2015-06-11 12:18:17 -0500

    Remove stop to test pedant failure.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Patrick Wright'
 maintainer_email 'patrick@chef.io'
 license          'all_rights'
 description      'Installs/Configures QA clusters'
-version          '1.0.1'
+version          '1.0.2'
 
 depends 'chef-server-cluster'
 depends 'omnibus-artifactory-artifact'
