df0ca7129e8f19096acacb62a32cf0f694f38512
Nathan Cerny <ncerny@chef.io>
2015-06-11 11:52:30 -0500

    Add destroy even when pedant fails, and sanity checking for nodes older than 24 hours

--- .delivery/build/recipes/provision_standalone_clean_aws.rb
+++ .delivery/build/recipes/provision_standalone_clean_aws.rb
@@ -68,54 +68,49 @@ end
 # rubocop:disable LineLength
 ruby_block 'see if cache exists' do
   block do
+    puts ''
     Dir.chdir path
-    nodes = JSON.parse(Mixlib::ShellOut.new('aws ec2 describe-instances --filters "Name=tag-value,Values=qa-chef-server-cluster-delivery-builder"').run_command.stdout)
+    nodes = JSON.parse(Mixlib::ShellOut.new('aws ec2 describe-instances --filters "Name=tag-value,Values=qa-chef-server-cluster-delivery-builder" "Name=tag-value,Values=standalone"').run_command.stdout)
     nodes['Reservations'].each do |reservation|
       reservation['Instances'].each do |instance|
-        puts instance['InstanceId']
+        if Time.now - Date.parse(instance['LaunchTime']).to_time > (24*60*60)
+          puts instance['InstanceId']
+        end
       end
     end
   end
 end
 
+ruby_block 'stand-up-machine' do
+  block do
+    Dir.chdir path
+    shell_out!("chef exec bundle exec chef-client --force-formatter -z -p 10257 -j #{attributes_file} -c #{repo_knife_file} -o qa-chef-server-cluster::standalone-server", live_stream: STDOUT, timeout: 7200)
+  end
+end
 
-#
-# ruby_block 'stand-up-machine' do
-#   block do
-#     Dir.chdir path
-#     shell_out!("chef exec bundle exec chef-client --force-formatter -z -p 10257 -j #{attributes_file} -c #{repo_knife_file} -o qa-chef-server-cluster::standalone-server", live_stream: STDOUT, timeout: 7200)
-#   end
-# end
-#
-# ruby_block 'run-pedant' do
-#   block do
-#     begin
-#       Chef::Log.fatal 'p-p-pedant'
-#       Dir.chdir path
-#       shell_out!("chef exec bundle exec chef-client --force-formatter -z -p 10257 -j #{attributes_file} -c #{repo_knife_file} -o qa-chef-server-cluster::standalone-server-test", live_stream: STDOUT, timeout: 7200)
-#     rescue Exception => e
-#       Chef::Log.fatal 'get excepted'
-#       Chef::Log.fatal e
-#       Chef::Log.fatal 'get excepted 2'
-#       Chef::Log.fatal e.class
-#       Chef::Log.fatal e.inspect
-#     end
-#   end
-#   ignore_failure true
-# end
-#
-# ruby_block 'get ips after' do
-#   block do
-#     Dir.chdir path
-#     shell_out!('chef exec bundle exec rake ip_search', live_stream: STDOUT, timeout: 7200)
-#   end
-# end
-#
-# # ruby_block 'destroy-machine' do
-# #   block do
-# #     shell_out!("chef exec bundle exec chef-client --force-formatter -z -p 10257 -j #{attributes_file} -c #{repo_knife_file} -o qa-chef-server-cluster::standalone-server-destroy", live_stream: STDOUT, timeout: 7200)
-# #   end
-# #   action :run
-# # end
-#
-# # rubocop:enable LineLength
+ruby_block 'run-pedant' do
+  block do
+    begin
+      node['pedant_success'] = true
+      Dir.chdir path
+      shell_out!("chef exec bundle exec chef-client --force-formatter -z -p 10257 -j #{attributes_file} -c #{repo_knife_file} -o qa-chef-server-cluster::standalone-server-test", live_stream: STDOUT, timeout: 7200)
+    rescue
+      node['pedant_failure'] = false
+      Chef::Log.fatal 'Pedant Failed!'
+    end
+  end
+  # Cannot, for the life of me, get 'notifies' to send message on pedant failure,
+  # so we will ignore failure and exit success or failure after machine is destroyed.
+  ignore_failure true
+end
+
+ruby_block 'destroy-machine' do
+  block do
+    shell_out!("chef exec bundle exec chef-client --force-formatter -z -p 10257 -j #{attributes_file} -c #{repo_knife_file} -o qa-chef-server-cluster::standalone-server-destroy", live_stream: STDOUT, timeout: 7200)
+  end
+  action :run
+end
+
+fail unless node['pedant_success']
+
+# rubocop:enable LineLength
