228a6f5a3c6cd0e8d284f90376b49ca433e23d2c
Nathan Cerny <ncerny@chef.io>
2015-06-11 09:31:27 -0500

    Fix rubocop.  Stub push to github and delivery-red-pill functionality

--- .delivery/build/recipes/provision.rb
+++ .delivery/build/recipes/provision.rb
@@ -4,94 +4,24 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-delivery_secrets = get_project_secrets
-path             = node['delivery']['workspace']['repo']
-cache            = node['delivery']['workspace']['cache']
-
-ssh_private_key_path =  File.join(cache, '.ssh', 'chef-server-build-key')
-ssh_public_key_path  =  File.join(cache, '.ssh', 'chef-server-build-key.pub')
-
-directory File.join(cache, '.ssh')
-directory File.join(cache, '.aws')
-
-file ssh_private_key_path do
-  sensitive true
-  content delivery_secrets['private_key']
-  owner node['delivery_builder']['build_user']
-  group node['delivery_builder']['build_user']
-  mode '0600'
-end
-
-file ssh_public_key_path do
-  content delivery_secrets['public_key']
-  owner node['delivery_builder']['build_user']
-  group node['delivery_builder']['build_user']
-  mode '0644'
-end
-
-attributes_file = File.join(cache, 'attributes.json')
-
-cookbook_file "attributes.json" do
-  path attributes_file
-  action :create
-end
-
-template File.join(cache, '.aws/config') do
-  sensitive true
-  source 'aws-config.erb'
-  variables(
-    aws_access_key_id: delivery_secrets['access_key_id'],
-    aws_secret_access_key: delivery_secrets['secret_access_key'],
-    region: delivery_secrets['region']
-  )
-end
-
-# TODO: support for other keys in qa-chef-server-cluster cookbook
+# if node['delivery']['change']['pipeline'] == 'master' &&
+#    node['delivery']['change']['stage'] == 'union'
+#   # This was lifted from delivery-truck publish
+#   secrets = get_project_secrets
+#   github_repo = node['delivery']['config']['delivery-truck']['publish']['github']
 #
-# template File.join(path, 'data_bags/secrets/lob-user-key.json') do
-#   sensitive true
-#   source 'lob-user-key.json.erb'
-#   variables(
-#     private_key_path: ssh_private_key_path,
-#     public_key_path:  ssh_public_key_path
-#   )
+#   delivery_github github_repo do
+#     deploy_key secrets['github']
+#     branch node['delivery']['change']['pipeline']
+#     remote_url "git@github.com:#{github_repo}.git"
+#     repo_path node['delivery']['workspace']['repo']
+#     cache_path node['delivery']['workspace']['cache']
+#     action :push
+#   end
 # end
+#
+# # By including this recipe we trigger a matrix of acceptance envs specified
+# # in the node attribute node['delivery-red-pill']['acceptance']['matrix']
+# include_recipe "delivery-red-pill::provision"
 
-repo_knife_file = File.join(path, ".chef/knife.rb")
-
-execute "chef exec rake prep" do
-  cwd path
-end
-
-# TODO how do we make sure we don't leave any nodes spun up
-
-ruby_block "stand-up-machine" do
-  block do
-    Dir.chdir path
-    #shell_out!("chef exec bundle exec chef-client --force-formatter -z -p 10257 -j #{attributes_file} -c #{repo_knife_file} -o qa-chef-server-cluster::standalone-server", {:live_stream => STDOUT, :timeout => 7200})
-  end
-end
-
-ruby_block "run-pedant" do
-  block do
-    begin
-      Chef::Log.fatal "p-p-pedant"
-      Dir.chdir path
-      shell_out!("chef exec bundle exec chef-client --force-formatter -z -p 10257 -j #{attributes_file} -c #{repo_knife_file} -o qa-chef-server-cluster::standalone-server-test", {:live_stream => STDOUT, :timeout => 7200})
-    rescue Exception => e
-      Chef::Log.fatal "get excepted"
-      Chef::Log.fatal e
-      Chef::Log.fatal "get excepted 2"
-      Chef::Log.fatal e.class
-      Chef::Log.fatal e.inspect
-    end
-  end
-  ignore_failure true
-end
-
-ruby_block "destroy-machine" do
-  block do
-    shell_out!("chef exec bundle exec chef-client --force-formatter -z -p 10257 -j #{attributes_file} -c #{repo_knife_file} -o qa-chef-server-cluster::standalone-server-destroy", {:live_stream => STDOUT, :timeout => 7200})
-  end
-  action :run
-end
+include_recipe 'build::provision_standalone_clean_aws'
